155b14d78fa2436693b1223f372a579bc0b64724
raman <khanahmadi.raman@yahoo.com>
2023-02-25 22:30:14 +0330

    firebase confige

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  priduction: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCKbMnzqETgmMoG1a7w1DUzXixYgSbErYQ',
+    authDomain: 'todo-list-7f2a1.firebaseapp.com',
+    projectId: 'todo-list-7f2a1',
+    storageBucket: 'todo-list-7f2a1.appspot.com',
+    messagingSenderId: '821815422983',
+    appId: '1:821815422983:web:0e46cfaff45b1544ecfaa2',
+  },
+};
